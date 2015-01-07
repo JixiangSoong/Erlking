ee41a1043fac69deb3c970b27131fd030f876f49
awen <zhou.vip@icloud.com>
2015-01-07 16:51:07 +0800

    1

--- src/BlockObj.ts
+++ src/BlockObj.ts
@@ -5,19 +5,10 @@ class BlockObj extends egret.Sprite{
 
     public constructor(){
         super();
-        this.createView();
+        this.createBlock();
     }
 
-    private createView():void {
-        this.textField = new egret.TextField();
-        this.addChild(this.textField);
-        this.textField.y = 300;
-        this.textField.width = 480;
-        this.textField.height = 100;
-        this.textField.textAlign = "center";
+    private createBlock():void {
     }
 
-    public setProgress(current, total):void {
-        this.textField.text = "游戏加载中..." + current + "/" + total;
-    }
 }
--- src/LoadingUI.ts
+++ src/LoadingUI.ts
@@ -29,7 +29,20 @@ class LoadingUI extends egret.Sprite{
 
     public constructor(){
         super();
-        this.createBlock();
+        this.createView();
+    }
+    private textField:egret.TextField;
+
+    private createView():void {
+        this.textField = new egret.TextField();
+        this.addChild(this.textField);
+        this.textField.y = 300;
+        this.textField.width = 480;
+        this.textField.height = 100;
+        this.textField.textAlign = "center";
     }
 
+    public setProgress(current, total):void {
+        this.textField.text = "游戏加载中..." + current + "/" + total;
+    }
 }
